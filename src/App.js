20c69be2a57e6381ae4dcb75abb2392c2b8f4bec
Nai Bhavik <116361800+naibhavik@users.noreply.github.com>
2023-02-25 09:43:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import React from 'react';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Title from './Components/Title';
+import Counter from './Components/Counter';
+
+
+function App() {
+
+  
+
+  
+
+  return (
+      <><Home />
+        <Counter />
+        </>
+          
+    
+  );
+
+  }
+export default App;
